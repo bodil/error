816f51bdf76fcdda365d1c9f2dd1ba5dcddfa7e9
Bodil Stokke <bodil@bodil.tv>
2013-05-01 09:05:16 +0200

    Bump to correct Red Lobster version.

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :license {:name "Apache License, version 2.0"
             :url "http://www.apache.org/licenses/LICENSE-2.0.html"}
   :plugins [[lein-cljsbuild "0.3.0"]]
-  :dependencies [[org.bodil/redlobster "0.2.0"]]
+  :dependencies [[org.bodil/redlobster "0.2.1"]]
   :profiles
   {:dev
    {:dependencies [[org.bodil/cljs-noderepl "0.1.8"]]
