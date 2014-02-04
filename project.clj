c08dc868e2a5c46f2235725e7e7494f1a07c8446
Bodil Stokke <bodil@bodil.org>
2014-02-04 14:27:26 +0000

    Bump deps and tag for 0.1.3 release.

--- project.clj
+++ project.clj
@@ -1,15 +1,16 @@
-(defproject org.bodil/error "0.1.2"
+(defproject org.bodil/error "0.1.3"
   :description "Async testing toolkit for ClojureScript"
   :url "https://github.com/bodil/error"
   :license {:name "Apache License, version 2.0"
             :url "http://www.apache.org/licenses/LICENSE-2.0.html"}
-  :plugins [[lein-cljsbuild "0.3.0"]]
-  :dependencies [[org.bodil/redlobster "0.2.1"]]
+  :plugins [[lein-cljsbuild "1.0.2"]]
+  :dependencies [[org.clojure/clojurescript "0.0-2156"]
+                 [org.bodil/redlobster "0.2.1"]]
   :profiles
   {:dev
-   {:dependencies [[org.bodil/cljs-noderepl "0.1.8"]]
-    :plugins [[org.bodil/lein-noderepl "0.1.8"]
-              [org.bodil/lein-error "0.1.2"]]
+   {:dependencies [[org.bodil/cljs-noderepl "0.1.10"]]
+    :plugins [[org.bodil/lein-noderepl "0.1.10"]
+              [org.bodil/lein-error "0.1.3"]]
     :repl-options {:nrepl-middleware [cemerick.piggieback/wrap-cljs-repl]}}}
   :cljsbuild
   {:builds {:test {:source-paths ["src" "test"]
